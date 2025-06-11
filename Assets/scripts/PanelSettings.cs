7d0ba36747834d68e3933e2ece8cdd0abb7090ea
Benjamin Hornung <benjamin.elias.hornung@t-online.de>
2025-06-11 18:48:49 +0200

    Added Credit-Screen

--- Assets/scripts/PanelSettings.cs
+++ Assets/scripts/PanelSettings.cs
@@ -2,15 +2,6 @@ using UnityEngine;
 
 public class PanelSettings : MonoBehaviour
 {
-    [SerializeField] public Vector3 cameraCenter;
-    [SerializeField] public float cameraSpeed = 30;
-    [SerializeField] public float cameraZoom = 15;
-    [SerializeField] public float cameraZoomSpeed = 5;
-    [SerializeField] public float limit_ZoomIn = 4;
-    [SerializeField] public float limit_ZoomOut = 25;
-    [SerializeField] public float relativeRotation = 0;
-    [SerializeField] public float limit_Left = -100;
-    [SerializeField] public float limit_Right = 100;
-    [SerializeField] public float limit_Top = 50;
-    [SerializeField] public float limit_Down = -50;
+    [SerializeField] public string type; //(same as the overlay Name)
+    [SerializeField] public bool isPlaced;
 }
